15d00ed28878a33b8e9a22f6259df6242583fe81
Bashir Elmaged <bashirgasm@gmail.com>
2022-11-07 06:35:07 -0600

    Update RecipientsDisplay.svelte

--- RecipientsDisplay.svelte
+++ RecipientsDisplay.svelte
@@ -1,3 +1,25 @@
 <script lang="ts">
-  export let recipients
+  export let recipients;
+    import RecipientsDisplay  from './AuditTable.svelte';
 </script>
+<div class="recipients">
+  <h2>{ recipients }</h2>
+  <RecipientsDisplay recipients={recipients}/>
+</div>
+<main>
+  <div class = "items">
+    {#each inventory as item}
+      <Item item={item}/>
+    {/each}
+  </div>
+
+  <p>There are { $itemsInCart } items in your cart.</p>
+  <p>Cart total: ${ $totalPrice }</p>
+  <p>Cart contents:</p>
+        
+  <ol>
+    {#each $cartContents as item}
+      <li>{item}</li>
+    {/each}
+  </ol>
+</main>
